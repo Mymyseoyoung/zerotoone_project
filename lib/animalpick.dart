0e6e3d06abe06304bb0f2da210185cb30fa6908b
Seoyoung Park <emilypark0721@gmail.com>
2025-01-13 18:15:32 +0900

    animal pick commit

--- lib/animalpick.dart
+++ lib/animalpick.dart
@@ -1,123 +1,127 @@
 import 'package:flutter/material.dart';
 
-class AnimalPick extends StatelessWidget {
+class AnimalPick extends StatefulWidget {
   const AnimalPick({super.key});
+
+  @override
+  State<AnimalPick> createState() => _AnimalPickState();
+}
+
+class _AnimalPickState extends State<AnimalPick> {
+  int? selectedAnimalIndex; // 선택된 동물 버튼의 인덱스
+  final animals = ['곰', '고양이', '강아지', '원숭이', '토끼']; // 동물 이름 목록
+  final images = [
+    'assets/images/bear.png',
+    'assets/images/cat.png',
+    'assets/images/dog.png',
+    'assets/images/monkey.png',
+    'assets/images/rabbit.png',
+  ]; // 동물 이미지 경로
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      backgroundColor: Colors.white, // 배경색을 흰색으로 설정
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: Align(
-          alignment: Alignment.centerLeft, // 제목을 왼쪽 정렬
-          child: Text(
-            '두몽이 만들기',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: size.width * 0.05,
-              fontWeight: FontWeight.w600,
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: size.height * 0.05), // 상단 여백
+            Text(
+              '두몽이 만들기',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: size.width * 0.055,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Pretendard',
+              ),
             ),
-          ),
-        ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start, // 텍스트를 왼쪽 정렬
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: size.height * 0.02),
-                Text(
-                  '함께 할 두몽이 종류를 골라볼까요?',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: size.width * 0.04,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                SizedBox(height: size.height * 0.02),
-              ],
+            SizedBox(height: size.height * 0.02),
+            Text(
+              '함께 할 두몽이 종류를 골라볼까요?',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: size.width * 0.045,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w400,
+                height: 1.2,
+              ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+            SizedBox(height: size.height * 0.03),
+            Expanded(
               child: GridView.builder(
-                itemCount: 6, // 6개의 버튼
+                itemCount: 6,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, // 한 줄에 2개의 버튼
-                  crossAxisSpacing: size.width * 0.05,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: size.width * 0.04,
                   mainAxisSpacing: size.height * 0.03,
                   childAspectRatio: 0.8,
                 ),
                 itemBuilder: (context, index) {
-                  if (index < 5) {
-                    // 동물 버튼
-                    final animals = ['곰', '고양이', '강아지', '원숭이', '토끼'];
-                    final images = [
-                      'assets/images/bear.png',
-                      'assets/images/cat.png',
-                      'assets/images/dog.png',
-                      'assets/images/monkey.png',
-                      'assets/images/rabbit.png'
-                    ];
-
+                  if (index < animals.length) {
                     return AnimalCard(
                       name: animals[index],
                       imagePath: images[index],
                       size: size,
+                      isSelected: selectedAnimalIndex == index,
                       onPressed: () {
-                        // 동물 버튼 클릭 시 동작
+                        setState(() {
+                          selectedAnimalIndex = index; // 선택된 버튼 인덱스 저장
+                        });
                       },
                     );
                   } else {
-                    // 마지막 빈 버튼
                     return EmptyAnimalCard(size: size);
                   }
                 },
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: size.width * 0.05,
-              vertical: size.height * 0.02,
-            ),
-            child: ElevatedButton(
-              onPressed: () {
-                // "선택하기" 버튼 클릭 시 동작 추가
-              },
+            ElevatedButton(
+              onPressed: selectedAnimalIndex != null
+                  ? () {
+                      // 선택하기 버튼 클릭 시 동작
+                      print("${animals[selectedAnimalIndex!]} 선택됨");
+                    }
+                  : null, // 선택이 없으면 비활성화
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
+                backgroundColor: selectedAnimalIndex != null
+                    ? const Color(0xFFFF5C35)
+                    : Colors.white,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(17),
                   side: BorderSide(
+                    color: selectedAnimalIndex != null
+                        ? const Color(0xFFFF5C35)
+                        : const Color(0xFFAEAEAE),
                     width: 2,
-                    color: const Color(0xFFAEAEAE),
                   ),
                 ),
                 padding: EdgeInsets.symmetric(vertical: size.height * 0.02),
               ),
-              child: Text(
-                '선택하기',
-                style: TextStyle(
-                  color: const Color(0xFF696969),
-                  fontSize: size.width * 0.045,
-                  fontFamily: 'Pretendard',
-                  fontWeight: FontWeight.w500,
-                  height: 1.4,
-                  letterSpacing: -0.56,
+              child: Center(
+                child: Text(
+                  selectedAnimalIndex != null
+                      ? '${animals[selectedAnimalIndex!]} 선택하기'
+                      : '선택하기',
+                  style: TextStyle(
+                    color: selectedAnimalIndex != null
+                        ? Colors.white
+                        : const Color(0xFF696969),
+                    fontSize: size.width * 0.045,
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w500,
+                    height: 1.4,
+                    letterSpacing: -0.56,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            SizedBox(height: size.height * 0.02),
+          ],
+        ),
       ),
     );
   }
@@ -127,12 +131,14 @@ class AnimalCard extends StatelessWidget {
   final String name;
   final String imagePath;
   final Size size;
+  final bool isSelected;
   final VoidCallback onPressed;
 
   const AnimalCard({
     required this.name,
     required this.imagePath,
     required this.size,
+    required this.isSelected,
     required this.onPressed,
     super.key,
   });
@@ -145,6 +151,10 @@ class AnimalCard extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: isSelected ? const Color(0xFFFF5C35) : Colors.transparent,
+            width: 2,
+          ),
           boxShadow: [
             BoxShadow(
               color: Colors.black.withOpacity(0.1),
